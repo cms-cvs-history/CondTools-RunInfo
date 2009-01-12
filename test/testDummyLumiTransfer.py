22d587e193e994f11e36c8125fd1b9f4b6ac970b
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-01-12 17:49:19 +0000

    added

---
+++ test/testDummyLumiTransfer.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.CondDBCommon.connect = 'sqlite_file:offlinelumi.db'
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    process.CondDBCommon,
+    timetype = cms.untracked.string('lumiid'),
+    logconnect = cms.untracked.string('sqlite_file:log.db'),
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string('LuminosityInfoRcd'),
+        tag = cms.string('lumitest')
+    ))
+)
+
+process.source = cms.Source("EmptyIOVSource",
+    lastValue = cms.uint64(1),
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(1),
+    interval = cms.uint64(1)
+)
+
+process.lumio2o = cms.EDAnalyzer("LuminosityPopConAnalyzer",
+    Source = cms.PSet(
+      lumiReaderName = cms.string('dummy'),
+      startRun = cms.untracked.int32(10),
+      numberOfRuns = cms.untracked.int32(2)
+    ),
+    SinceAppendMode = cms.bool(True),
+    name = cms.untracked.string('LuminosityInfo'),
+    record = cms.string('LuminosityInfoRcd'),
+    loggingOn = cms.untracked.bool(False),
+    debug = cms.bool(False)
+)
+
+process.p = cms.Path(process.lumio2o)
---
+++ test/testOMDSLumiTransfer.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.CondDBCommon.connect = 'sqlite_file:offlinelumi.db'
+process.CondDBCommon.DBParameters.authenticationPath = '/afs/cern.ch/user/x/xiezhen'
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    process.CondDBCommon,
+    timetype = cms.untracked.string('lumiid'),
+    logconnect = cms.untracked.string('sqlite_file:log.db'),
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string('LuminosityInfoRcd'),
+        tag = cms.string('lumitest')
+    ))
+)
+
+process.source = cms.Source("EmptyIOVSource",
+    lastValue = cms.uint64(1),
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(1),
+    interval = cms.uint64(1)
+)
+
+process.lumio2o = cms.EDAnalyzer("LuminosityPopConAnalyzer",
+    Source = cms.PSet(
+      lumiReaderName = cms.string('omds'),
+      startRun = cms.untracked.int32(70674),
+      numberOfRuns = cms.untracked.int32(1),
+      connect = cms.string('oracle://cms_omds_lb/CMS_LUMI'),
+      authenticationPath = cms.string('/afs/cern.ch/user/x/xiezhen')
+    ),
+    SinceAppendMode = cms.bool(False),
+    name = cms.untracked.string('LuminosityInfo'),
+    record = cms.string('LuminosityInfoRcd'),
+    loggingOn = cms.untracked.bool(False),
+    debug = cms.bool(True)
+)
+
+process.p = cms.Path(process.lumio2o)
